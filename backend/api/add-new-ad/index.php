97b3456169dcc49db1573e12dfc386bac3f9bfe7
Mikael Nilsson <mikaelnilsson@hotmail.com>
2019-04-21 14:01:07 +0200

    Add even more validation methods.

--- backend/api/add-new-ad/index.php
+++ backend/api/add-new-ad/index.php
@@ -5,47 +5,26 @@ header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
 //header('Content-Type: application/json; charset=utf-8');
 
-//require_once './validate.php';
 require_once '../../settings.php';
 require_once '../../classes/db.php';
+require_once '../../classes/validate.php';
 
-$data_o = json_decode(file_get_contents('php://input'));
-
-print_r($data_o);
+$validate_o = new Validate();
 $db_o = new DB($settings_a);
-echo $db_o->add($data_o);
 
-// $bytes = random_bytes(5);
-// var_dump(bin2hex($bytes));
-//$validationErrors_as = validate($data_am);
+$newAd_o = json_decode(file_get_contents('php://input'));
 
-
-// print_r($validationErrors_as);
-// print_r($data_am);
-// sleep(2);
-
-//echo '{"status":"ok","ad":"' . $data_o->ad . '","county":' . $data_o->county . '}';
-// echo json_encode($data, JSON_UNESCAPED_UNICODE);
-
-
-// ad: "En bok av Strindberg. En gigant, bland giganter."
-// category: 8
-// county: 14
-// email: "mikaelnilsson1973@hotmail.com"
-// heading: "Röda rummet"
-// image: undefined
-// phone: "730725069"
-// price: 123
-
-
-
-
-
-
-
-
-
-
-
+$successfulValidation_m = $validate_o->validateAll($newAd_o);
+if($successfulValidation_m) {
+    $db_o->add($newAd_o);
+    $success_o = new stdClass();
+    $success_o->status = 'ok';
+    echo json_encode($success_o, JSON_UNESCAPED_UNICODE);
+} else {
+    $error_o = new stdClass();
+    $error_o->status = 'error';
+    $error_o->description = $newAd_o;
+    echo json_encode($error_o, JSON_UNESCAPED_UNICODE);
+}
 
 ?>
--- backend/classes/validate.php
+++ backend/classes/validate.php
@@ -1,22 +1,51 @@
 <?php
 
+/**
+ * Validate is a class that has methods for validating the input from the
+ * buy-and-sell web site.
+ * 
+ * Example usage:
+ * 
+ * $validate_o = new Validate();
+ * $validate_o->validateAll($input_o);
+ * 
+ * @author Mikael Nilsson <mikael@digizone.se>
+ * @access public
+ */
 class Validate {
-
-    private $pdo_o;
 
     function __construct() {
     }
 
+    /**
+     * Validate all input properties at once.
+     * 
+     * @param object $params_o Object that should contain the following properties:
+     *                          
+     *                          body - String (1 - 400 characters)
+     *                          category - Integer (1 - 31)
+     *                          county - Integer (1 - 23)
+     *                          header - String (1 - 200 characters)
+     *                          price - Integer (>= 0)
+     *                          type - String ("buy" or "sell")
+     * 
+     * @return mixed Returns boolean false if any of the properties doesn't validate,
+     *               otherwise returns true.
+     */
     public function validateAll($params_o) {
         
-        $successfulValidation_m = true;
-        $successfulValidation_m = $params_o->body = isset($params_o->body) ? $this->validateBody($params_o->body) : false;
-        $successfulValidation_m = $params_o->category = isset($params_o->category) ? $this->validateCategory($params_o->category) : false;
-        $successfulValidation_m = $params_o->county = isset($params_o->county) ? $this->validateCounty($params_o->county) : false;
-        $successfulValidation_m = $params_o->header = isset($params_o->header) ? $this->validateHeader($params_o->header) : false;
-        $successfulValidation_m = $params_o->price = isset($params_o->price) ? $this->validateHeader($params_o->price) : false;
-        $successfulValidation_m = $params_o->type = isset($params_o->type) ? $this->validateType($params_o->type) : false;
-        return $successfulValidation_m;
+        $params_o->body = isset($params_o->body) ? $this->validateBody($params_o->body) : false;
+        $params_o->category = isset($params_o->category) ? $this->validateCategory($params_o->category) : false;
+        $params_o->county = isset($params_o->county) ? $this->validateCounty($params_o->county) : false;
+        $params_o->header = isset($params_o->header) ? $this->validateHeader($params_o->header) : false;
+        $params_o->price = isset($params_o->price) ? $this->validatePrice($params_o->price) : false;
+        $params_o->type = isset($params_o->type) ? $this->validateType($params_o->type) : false;
+        foreach($params_o as $prop_m) {
+            if($prop_m === false) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public function validateBody($body_m) {
@@ -71,7 +100,7 @@ class Validate {
         if(!is_int($price_m)) {
             return false;
         }
-        if($price_m < 0) {
+        if($price_m < 1) {
             return false;
         }
         return $price_m;
@@ -82,13 +111,11 @@ class Validate {
         if(!is_string($type_m)) {
             return false;
         }
-        if($type_m !== 'buy' || $type_m !== 'sell') {
+        if(!($type_m === 'buy' || $type_m === 'sell')) {
             return false;
         }
         return $type_m;
     }
-
-
 }
 
 ?>
