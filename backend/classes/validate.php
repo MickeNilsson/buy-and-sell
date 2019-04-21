e33571f5c0347bd17356f16583dd416084a420ab
Mikael Nilsson <mikaelnilsson@hotmail.com>
2019-04-21 12:25:51 +0200

    Add validation methods.

--- backend/classes/validate.php
+++ backend/classes/validate.php
@@ -9,20 +9,26 @@ class Validate {
 
     public function validateAll($params_o) {
         
-        $params_o->type = $this->validateType($params_o->type);
-        $params_o->category = $this->validateCategory($params_o->category);
-        $params_o->county = $this->validateCounty($params_o->county);
+        $successfulValidation_m = true;
+        $successfulValidation_m = $params_o->body = isset($params_o->body) ? $this->validateBody($params_o->body) : false;
+        $successfulValidation_m = $params_o->category = isset($params_o->category) ? $this->validateCategory($params_o->category) : false;
+        $successfulValidation_m = $params_o->county = isset($params_o->county) ? $this->validateCounty($params_o->county) : false;
+        $successfulValidation_m = $params_o->header = isset($params_o->header) ? $this->validateHeader($params_o->header) : false;
+        $successfulValidation_m = $params_o->price = isset($params_o->price) ? $this->validateHeader($params_o->price) : false;
+        $successfulValidation_m = $params_o->type = isset($params_o->type) ? $this->validateType($params_o->type) : false;
+        return $successfulValidation_m;
     }
 
-    public function validateType($type_m) {
+    public function validateBody($body_m) {
 
-        if(!is_string($type_m)) {
+        if(!is_string($body_m)) {
             return false;
         }
-        if($type_m !== 'buy' || $type_m !== 'sell') {
+        $length_i = strlen($body_m);
+        if($length_i < 1 || $length_i > 400) {
             return false;
         }
-        return $type_m;
+        return $body_m;
     }
 
     public function validateCategory($category_m) {
@@ -45,6 +51,41 @@ class Validate {
             return false;
         }
         return $county_m;
+    }
+
+    public function validateHeader($header_m) {
+
+        if(!is_string($header_m)) {
+            return false;
+        }
+        $length_i = strlen($header_m);
+        if($length_i < 1 || $length_i > 200) {
+            return false;
+        }
+        return $header_m;
+    }
+
+    public function validatePrice($price_m) {
+
+        $price_m = intval($price_m);
+        if(!is_int($price_m)) {
+            return false;
+        }
+        if($price_m < 0) {
+            return false;
+        }
+        return $price_m;
+    }
+
+    public function validateType($type_m) {
+
+        if(!is_string($type_m)) {
+            return false;
+        }
+        if($type_m !== 'buy' || $type_m !== 'sell') {
+            return false;
+        }
+        return $type_m;
     }
 
 
