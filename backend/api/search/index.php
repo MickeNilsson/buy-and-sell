b5855b900fed42c3e5e8be82f90159a21ace0c19
Mikael Nilsson <mikaelnilsson@hotmail.com>
2019-07-26 20:01:44 +0200

    Disable escaping of unicode characters in response

--- backend/api/search/index.php
+++ backend/api/search/index.php
@@ -32,7 +32,7 @@ $db_o = new DB($settings_a);
 $search_o = json_decode(file_get_contents('php://input'));
 $sql_s = $db_o->search($search_o);
 $search_o->sql = $sql_s;
-echo json_encode($search_o);
+echo json_encode($search_o, JSON_UNESCAPED_UNICODE);
 
 
 
